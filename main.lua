d8d8300dad4468972814d18daeca4427c9991547
PizzaPoot <viira.mihkel@gmail.com>
2023-11-01 15:56:06 +0200

    main

--- main.lua
+++ main.lua
@@ -1,21 +1,18 @@
 local mod = RegisterMod("sitt", 1)
 local enemyCount = 0
-
-function mod:onEnemyDeath()
+local bosscount = 0
+function mod:onEnemyDeath(enemy)
+    print(enemy.EntityNPC.IsBoss())
     enemyCount = enemyCount + 1
 end
 
 Isaac.DebugString("Mod initialized")
 
 
-function mod:GameTick()
-    Isaac.ConsoleOutput(tostring(enemyCount))
-    print(enemyCount)
+function mod:render()
+    Isaac.RenderText("Killed enemies: " .. enemyCount, 100, 100, 255, 255, 255, 255)
+    Isaac.RenderText("Killed bosses: " .."NaN", 100, 90, 255, 255, 255, 255)
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, function()
-    Isaac.RenderText("Killed enemies: " .. enemyCount, 100, 100, 255, 255, 255, 255)
-end)
-
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.GameTick)
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
 mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, mod.onEnemyDeath)
