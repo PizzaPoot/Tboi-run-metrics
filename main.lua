f47ad2d98401329e118a17fcb55456643ca162d0
PizzaPoot <viira.mihkel@gmail.com>
2023-11-14 15:38:59 +0200

    maybe got bosscounter all done?

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
 local mod = RegisterMod("sitt", 1)
 local enemyCount = 0
 local bosscount = 0
+local enemyparent = "404"
 function mod:onEnemyDeath(enemy)
---    if enemy:IsBoss() == true and enemy:GetLastParent() ~= "nil" then
-    if enemy:IsBoss() == true and Game():GetRoom():GetType() ~= RoomType.ROOM_MINIBOSS then
+    if enemy:IsBoss() == true and Game():GetRoom():GetType() ~= RoomType.ROOM_MINIBOSS and enemy:GetLastParent():IsDead() then
         bosscount = bosscount + 1
     else
         enemyCount = enemyCount + 1
     end
-    
+
 
 end
 
@@ -17,10 +17,8 @@ Isaac.DebugString("Mod initialized")
 
 
 function mod:render()
---  local room = Game():GetRoom():GetType() == RoomType.ROOM_MINIBOSS
     Isaac.RenderText("Killed enemies: " .. enemyCount, 100, 100, 255, 255, 255, 255)
     Isaac.RenderText("Killed bosses: " ..bosscount, 100, 90, 255, 255, 255, 255)
---  Isaac.RenderText("sitt: " ..tostring(room), 100, 80, 255, 255, 255, 255)
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
