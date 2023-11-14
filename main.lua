9327d91a1e2af6197c844733f614de4395b4ddb4
PizzaPoot <viira.mihkel@gmail.com>
2023-11-14 15:20:40 +0200

    maybe fixed minibosses from counting bosskills

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local mod = RegisterMod("sitt", 1)
 local enemyCount = 0
 local bosscount = 0
 function mod:onEnemyDeath(enemy)
-    if enemy:IsBoss() == true then
+    if enemy:IsBoss() == true and Game():GetRoom():GetType() ~= RoomType.ROOM_MINIBOSS then
         bosscount = bosscount + 1
     else
         enemyCount = enemyCount + 1
@@ -13,8 +13,10 @@ Isaac.DebugString("Mod initialized")
 
 
 function mod:render()
+--  local room = Game():GetRoom():GetType() == RoomType.ROOM_MINIBOSS
     Isaac.RenderText("Killed enemies: " .. enemyCount, 100, 100, 255, 255, 255, 255)
     Isaac.RenderText("Killed bosses: " ..bosscount, 100, 90, 255, 255, 255, 255)
+--  Isaac.RenderText("sitt: " ..tostring(room), 100, 80, 255, 255, 255, 255)
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
