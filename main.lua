1cdbac86ee32d36074f608c63817186832e5f65b
PizzaPoot <viira.mihkel@gmail.com>
2023-11-29 15:07:55 +0200

    Started with time and data saving

--- main.lua
+++ main.lua
@@ -1,39 +1,55 @@
 local mod = RegisterMod("sitt", 1)
 local enemyCount = 0
 local bosscount = 0
+local runstartedtime = 0
+local runtime = 0
+local totalruntime = 0
+
+
 function mod:onEnemyDeath(enemy)
     if CurrentRoom:GetType() == RoomType.ROOM_BOSS and CurrentRoom:GetAliveEnemiesCount() == 1 then
-        bosscount = bosscount + 1
+        bosscount = bosscount + 1 --counts boss kills only in the boss room
     elseif enemy:GetBossID() == 0 then
-        enemyCount = enemyCount + 1
+        enemyCount = enemyCount + 1 --wont count bosses in not boss rooms
     end
-    Isaac.ConsoleOutput(tostring(enemy:GetBossID()))
-    Isaac.ConsoleOutput(tostring(enemy:IsBoss()))
-    --[[
-    Enembid = enemy:GetBossID()
-    if enemy:IsBoss() == true and enemy:GetBossID() ~= 0 then
-        bosscount = bosscount + 1
-    else
-        enemyCount = enemyCount + 1
-    end
-    --]]
 end
 
+
+function mod:runstarted(_, continue)
+    runstartedtime = Isaac.GetTime()
+    if continue == false then
+        enemyCount = 0
+        bosscount = 0
+    else
+        Isaac.ConsoleOutput("\n loading save data")
+        --load save data
+    end
+end
+
+
+function mod:runended(_, died)
+    runtime = Isaac.GetTime() - runstartedtime
+    totalruntime = totalruntime + runtime
+    Isaac.ConsoleOutput("\n run ended  saving data")
+    --save data
+end
+
+
+function mod:exitedrun()
+    runtime = Isaac.GetTime() - runstartedtime
+    totalruntime = totalruntime + runtime
+    Isaac.ConsoleOutput("\n exited run")
+    --save data
+end
 
 
 function mod:onRoomCleared()
     CurrentRoom = Game():GetRoom()
 end
---[[
-    local entities = Isaac.GetRoomEntities()
-    for i = 1, #entities do
-        if entities[i].Type == EntityType.ENTITY_LARRYJR then
-            return true
-        end
-    end
-    return false
---]]
-Isaac.DebugString("Mod initialized")
+
+
+
+Isaac.DebugString("Run history initialized")
 
 
 function mod:render()
@@ -44,4 +60,6 @@ end
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
 mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, mod.onEnemyDeath)
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onRoomCleared)
---mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.gametick)
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.runstarted)
+mod:AddCallback(ModCallbacks.MC_POST_GAME_END, mod.runended)
+mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.exitedrun)
