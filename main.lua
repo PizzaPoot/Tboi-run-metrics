f8ef68c39944aac97e60be2e7be52522419ecaf2
PizzaPoot <viira.mihkel@gmail.com>
2023-11-29 15:33:51 +0200

    Stuff :thumbsup:

--- main.lua
+++ main.lua
@@ -7,7 +7,8 @@ local totalruntime = 0
 
 
 function mod:onEnemyDeath(enemy)
-    if CurrentRoom:GetType() == RoomType.ROOM_BOSS and CurrentRoom:GetAliveEnemiesCount() == 1 then
+    Aliveenemies = CurrentRoom:GetAliveEnemiesCount()
+    if CurrentRoom:GetType() == RoomType.ROOM_BOSS and CurrentRoom:GetAliveEnemiesCount() <= 1 then
         bosscount = bosscount + 1 --counts boss kills only in the boss room
     elseif enemy:GetBossID() == 0 then
         enemyCount = enemyCount + 1 --wont count bosses in not boss rooms
@@ -38,7 +39,7 @@ end
 function mod:exitedrun()
     runtime = Isaac.GetTime() - runstartedtime
     totalruntime = totalruntime + runtime
-    Isaac.ConsoleOutput("\n exited run")
+    Isaac.ConsoleOutput("\n exited run, runtime: " .. runtime / 1000 .. "sec" .. ", total runtime: " .. totalruntime / 1000 .."sec")
     --save data
 end
 
@@ -55,6 +56,7 @@ Isaac.DebugString("Run history initialized")
 function mod:render()
     Isaac.RenderText("enemies killed: " ..enemyCount, 100, 100, 255, 255, 255, 255)
     Isaac.RenderText("bosses killed:  " ..bosscount, 100, 90, 255, 255, 255, 255)
+    Isaac.RenderText("AliveEnemies:  " ..tostring(Aliveenemies), 100, 80, 255, 255, 255, 255)
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
